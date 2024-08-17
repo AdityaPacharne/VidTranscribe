38f51a3411b5cd78c5893b95627681fedf8b8d26
Aditya Pacharne <48561911+AdityaPacharne@users.noreply.github.com>
2024-08-17 12:37:50 +0530

    Fixed errors regarding torch and numpy versions, trying to fix the make command error

--- vidtranscribe.py
+++ vidtranscribe.py
@@ -1,96 +1,109 @@
+# Importing packages that are already installed
 import subprocess
 import platform
 import os
 import sys
 from datetime import datetime
-import tkinter as tk
-from tkinter import filedialog, messagebox, ttk
-subprocess.check_call([sys.executable, "-m", "pip", "install", "Pillow"])
-from PIL import Image, ImageTk
-from multiprocessing import Process
 
+# Checking type of OS
 os_name = platform.system()
+architecture_name = os.uname().machine
+
+# Created address variables
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 WHISPER_DIR = os.path.join(BASE_DIR, "whisper.cpp")
 TESTS_DIR = os.path.join(BASE_DIR, "tests")
 os.makedirs(TESTS_DIR, exist_ok=True)
 
+# Function to install libraries
 def install_libraries():
-    base_libraries = ["Pillow", "transformers", "openai", "tk", "requests"]
+    base_libraries = ["Pillow", "numpy==1.23.5", "torch==2.2.0", "transformers", "openai", "tk", "requests"]
     macos_arm_libraries = ["coremltools", "ane_transformers"]
-    machine_type = os.uname().machine
-    required_libraries = base_libraries + macos_arm_libraries if os_name == "Darwin" and "arm" in machine_type else base_libraries
+    if os_name == "Darwin" and "arm" in architecture_name:
+        required_libraries = base_libraries + macos_arm_libraries
+    else:
+        required_libraries = base_libraries
     subprocess.check_call([sys.executable, "-m", "pip", "install", *required_libraries])
     try:
         subprocess.check_call([sys.executable, "-m", "pip", "install", "git+https://github.com/openai/whisper.git"])
     except subprocess.CalledProcessError:
         messagebox.showerror("Error", "Failed to install Whisper library.")
         sys.exit(1)
+install_libraries()
 
-def install_ffmpeg():
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+from PIL import Image, ImageTk
+
+# Function to install packages
+def install_packages():
     if os_name == "Linux":
         subprocess.check_call(["sudo", "apt-get", "install", "-y", "ffmpeg", "ccache", "vlc"])
     elif os_name == "Darwin":
+        # Checks if Homebrew is installed, if not install it
+        try:
+            subprocess.check_call(["brew", "--version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            print("Homebrew is already installed.")
+        except subprocess.CalledProcessError:
+            print("Homebrew not found. Installing...")
+            subprocess.check_call(
+                ["/bin/bash", "-c", "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"]
+            )
+        except FileNotFoundError:
+            print("Homebrew not found. Installing...")
+            subprocess.check_call(
+                ["/bin/bash", "-c", "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"]
+            )
         subprocess.check_call(["brew", "install", "ffmpeg", "ccache"])
-        subprocess.check_call(["brew", "install", "--cask", "vlc"])
+        # subprocess.check_call(["brew", "install", "--cask", "vlc"])
     elif os_name == "Windows":
-        messagebox.showwarning("Manual Installation Required", "Please download and install ffmpeg manually from https://www.gyan.dev/ffmpeg/builds/")
+        messagebox.showwarning("Manual Installation Required", "Please download and install VLC and ffmpeg manually from VLC Official site and https://www.gyan.dev/ffmpeg/builds/")
         sys.exit(1)
-    else:
-        messagebox.showerror("Error", "Unsupported OS for automatic ffmpeg installation.")
-        sys.exit(1)
+install_packages()
 
-def check_ffmpeg():
-    try:
-        subprocess.run(['ffmpeg', '-version'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
-    except subprocess.CalledProcessError:
-        install_ffmpeg()
-
+# Clones the whisper.cpp github repo
 def clone_whisper():
     if not os.path.exists(WHISPER_DIR):
         subprocess.run(["git", "clone", "https://github.com/ggerganov/whisper.cpp.git", WHISPER_DIR], check=True)
-
-def ffmpeg_and_cloning():
-    check_ffmpeg()
-    clone_whisper()
-
-def parallel():
-    p1 = Process(target=install_libraries)
-    p2 = Process(target=ffmpeg_and_cloning)
-    p1.start()
-    p2.start()
-    p1.join()
-    p2.join()
-
-def set_compilation_flags():
-    architecture = platform.machine()
-    cflags = "-Iggml/include -Iggml/src -Iinclude -Isrc -Iexamples -D_XOPEN_SOURCE=600 -D_GNU_SOURCE -DNDEBUG -DGGML_USE_OPENMP -std=c11 -fPIC -O3 -Wall -Wextra -Wpedantic -Wcast-qual -Wno-unused-function -Wshadow -Wstrict-prototypes -Wpointer-arith -Wmissing-prototypes -Werror=implicit-int -Werror=implicit-function-declaration -pthread -fopenmp -Wdouble-promotion"
-    cxxflags = "-std=c++11 -fPIC -O3 -Wall -Wextra -Wpedantic -Wcast-qual -Wno-unused-function -Wmissing-declarations -Wmissing-noreturn -pthread -fopenmp -Wno-array-bounds -Wno-format-truncation -Wextra-semi -Iggml/include -Iggml/src -Iinclude -Isrc -Iexamples -D_XOPEN_SOURCE=600 -D_GNU_SOURCE -DNDEBUG -DGGML_USE_OPENMP"
-    cflags += " -mcpu=native" if architecture == "aarch64" else " -mcpu=cortex-a57"
-    cxxflags += " -mcpu=native" if architecture == "aarch64" else " -mcpu=cortex-a57"
-    os.environ['CFLAGS'] = cflags
-    os.environ['CXXFLAGS'] = cxxflags
+clone_whisper()
 
 def build_whisper():
-    set_compilation_flags()
+    cflags = "-Iggml/include -Iggml/src -Iinclude -Isrc -Iexamples -D_XOPEN_SOURCE=600 -D_GNU_SOURCE -DNDEBUG -DGGML_USE_OPENMP -std=c11 -fPIC -O3 -Wall -Wextra -Wpedantic -Wcast-qual -Wno-unused-function -Wshadow -Wstrict-prototypes -Wpointer-arith -Wmissing-prototypes -Werror=implicit-int -Werror=implicit-function-declaration -pthread -fopenmp -Wdouble-promotion -mcpu=native"
+    cxxflags = "-std=c++11 -fPIC -O3 -Wall -Wextra -Wpedantic -Wcast-qual -Wno-unused-function -Wmissing-declarations -Wmissing-noreturn -pthread -fopenmp -Wno-array-bounds -Wno-format-truncation -Wextra-semi -Iggml/include -Iggml/src -Iinclude -Isrc -Iexamples -D_XOPEN_SOURCE=600 -D_GNU_SOURCE -DNDEBUG -DGGML_USE_OPENMP -mcpu=native"
+
     os.chdir(WHISPER_DIR)
-    env = os.environ.copy()
-    env['WHISPER_COREML'] = '1'
-    subprocess.run(["make", "clean"], env=env, check=True)
-    subprocess.run(['make', '-j'], env=env, check=True)
+    command = (
+        f'export CFLAGS="{cflags}"; '
+        f'export CXXFLAGS="{cxxflags}"; '
+    )
+    subprocess.run(command, shell=True, check=True)
 
 def browse_file():
     filepath = filedialog.askopenfilename()
+
     if filepath:
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         model_name = model_var.get()
         model_path = os.path.join(WHISPER_DIR, "models", f"ggml-{model_name}.bin")
+
         if not os.path.exists(model_path):
             download_model(model_name)
-        if sys.platform == "darwin" and "arm" in os.uname().machine:
+
+        env = os.environ.copy()
+
+        if sys.platform == "darwin" and "arm" in architecture_name:
             if not os.path.exists(os.path.join(WHISPER_DIR, "models", f"coreml-{model_name}.mlmodel")):
                 generate_coreml_model(model_name)
+                os.chdir(WHISPER_DIR)
+                build_whisper()
+                subprocess.run(['make', 'clean'], env=env, check=True)
+                env['WHISPER_COREML'] = '1'
+                subprocess.run(['make', '-j'], env=env, check=True)
+
+        else:
             build_whisper()
+            subprocess.run(['make'], env=env, check=True)
+
         convert_to_wav(filepath, timestamp)
         extract_text(timestamp, model_name)
         play_video_with_subtitles(filepath, os.path.join(TESTS_DIR, f"{timestamp}.wav.srt"))
@@ -136,22 +149,25 @@ def play_video_with_subtitles(video_path, subtitles_path):
         messagebox.showerror("Error", str(e))
 
 if __name__ == "__main__":
-    parallel()
     models = ['tiny.en', 'base.en', 'small.en', 'medium.en', 'large.en']
+
     root = tk.Tk()
-    window_width = 800  
-    window_height = 600  
-    root.geometry(f"{window_width}x{window_height}")  
+    window_width = 800
+    window_height = 600
+    root.geometry(f"{window_width}x{window_height}")
+
     image = Image.open(os.path.join(BASE_DIR, "image.jpg"))
     photo = ImageTk.PhotoImage(image)
     background_label = tk.Label(root, image=photo)
     background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
     model_var = tk.StringVar(value=models[0])
     model_label = tk.Label(root, text="Select Model:")
     model_label.place(relx=0.5, rely=0.5, anchor='center', y=-40)
     model_dropdown = ttk.Combobox(root, textvariable=model_var, values=models, state='readonly')
     model_dropdown.place(relx=0.5, rely=0.5, anchor='center', y=-20)
-    browse_button = tk.Button(root, text="Browse", command=browse_file)
-    browse_button.place(relx=0.5, rely=0.7, anchor='center')  
-    root.mainloop()
 
+    browse_button = tk.Button(root, text="Browse", command=browse_file)
+    browse_button.place(relx=0.5, rely=0.7, anchor='center')
+
+    root.mainloop()
